ec3acc706cc3097f9f83381b0ea2c4ec8965df0f
LuisOtero <luisoterousa00@gmail.com>
2023-06-23 11:37:14 -0500

    final

--- src/components/CheckOut/CheckOut.js
+++ src/components/CheckOut/CheckOut.js
@@ -1,9 +1,11 @@
 import React from "react";
+import '../CheckOut/CheckOut.css';
 
 const CheckOut = () => {
   return (
-    <div>
+    <div className="checkout">
       <h3>Gracias por tu compra!</h3>
+      <img src="https://hotstuffproducciones.com/wp-content/uploads/Lo-sentimos-el-tiempo-de-compra-ha-expirado-4.png"></img>
     </div>
   );
 };
