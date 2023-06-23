2eec452fe2f683b61698b5af90d1b01c8310786b
LuisOtero <luisoterousa00@gmail.com>
2023-06-23 13:18:37 -0500

    Check Out

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react"
 import { getProducts, getProductsByCategory } from '../../asyncMock'
 import ItemList from "../ItemList/ItemList"
 import { useParams } from "react-router-dom"
+import { getDocs, collection,query,where } from "firebase/firestore"
+import { db } from "../../services/firebase/firebaseConfig"
 
 const ItemListContainer = ({ greeting}) => {
    const [products, setProducts] = useState([])
